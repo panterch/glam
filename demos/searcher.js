1995d640e39c60f198814c05264a39b921d81553
Beat Seeliger <seb@panter.ch>
2015-02-27 20:05:14 +0100

    output all citizens

--- demos/searcher.js
+++ demos/searcher.js
@@ -19,28 +19,34 @@ var claimRequest = initialSearchResults.then( function(wdId) {
   return breq.get(url);
 });
 
-var personIdRequest = claimRequest.then( function(response) {
+var entityIdRequest = claimRequest.then(function(response) {
   var items = response.body.items;
-  return items[Math.floor(Math.random()*items.length)];
+  // return items[Math.floor(Math.random()*items.length)];
+  items.forEach(function(entityId) {
+    requestEntity(entityId);
+  });
 });
 
-var personRequest = personIdRequest.then(function(personId) {
-  var url = wdk.getEntities([personId], 'de');
-  return breq.get(url);
-});
-
-personRequest.then(function(response) {
-	brancher(response);
-});
+var requestEntity = function(entityId) {
+  var url = wdk.getEntities([entityId], 'de');
+  breq.get(url).then(function(response) {
+    brancher(response);
+  });
+}
 
 var handleHuman = function(response) {
   var personId = Object.keys(response.body.entities)[0];
   var result = response.body.entities[personId];
-  var sentence = 'Dort lebte ' +
-    result.labels.de.value +
-    ' ein(e) ' +
-    result.descriptions.de.value +
-    '.\nEnde.';
+  var sentence = "Unbekannte Person";
+  if (result.labels && result.labels.de) {
+    sentence = result.labels.de.value;
+  }
+  if (result.descriptions && result.descriptions.de) {
+    sentence = sentence +
+      ' war/ist ein/e ' +
+      result.descriptions.de.value
+  }
+  sentence = sentence + '.';
   console.log(sentence);
 }
 
@@ -49,7 +55,12 @@ var brancher = function(response) {
     5: handleHuman
   }
   var firstEntityKey = Object.keys(response.body.entities)[0];
-	var firstEntity = response.body.entities[firstEntityKey];
+  var firstEntity = response.body.entities[firstEntityKey];
   var instanceOf = firstEntity.claims['P31'][0].mainsnak.datavalue.value['numeric-id'];
-  strategies[instanceOf](response);
+  var strategy = strategies[instanceOf];
+  if (strategy) {
+    strategy(response);
+  } else {
+    console.log('No strategy for', instanceOf);
+  }
 }
